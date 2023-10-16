ec84cafaf39f66b495f2ab80b1632a7f5f372547
Markus Ketterer <markus.ketterer@gugler.at>
2023-10-16 09:27:16 +0200

    fix variables; set typoscript

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,8 @@
+<?php
+defined("TYPO3") || die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+  "gugler_powermail",
+  "Configuration/TypoScript",
+  "gugler* powermail a11y improvements"
+);
