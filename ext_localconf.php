28e77f37c5128adf8a707eecead6d83115b0f92f
Markus Ketterer <markus.ketterer@gugler.at>
2024-09-25 13:57:59 +0200

    XClass powermail Field

---
+++ ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+
+//xclass for Field
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\In2code\Powermail\Domain\Model\Field::class] = [
+    'className' => \Gugler\GuglerPowermail\Domain\Model\Field::class,
+];
