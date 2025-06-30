c3aacd81e15b3409b0f8c55a42febce8f83f7f4a
HAME, Michael Hammerl <michael.hammerl@gugler.at>
2025-06-30 19:52:42 +0200

    make captcha multi lang ready

--- Classes/ViewHelpers/GuglerCaptchaViewHelper.php
+++ Classes/ViewHelpers/GuglerCaptchaViewHelper.php
@@ -6,6 +6,7 @@ use In2code\Powermail\Domain\Model\Field;
 use In2code\Powermail\Utility\MathematicUtility;
 use In2code\Powermail\Utility\SessionUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
 /**
  * Class CaptchaViewHelper
@@ -17,18 +18,6 @@ class GuglerCaptchaViewHelper extends AbstractTagBasedViewHelper {
       'x' => "mal"
     );
 
-    private $numbers = array(
-      "1" => "Eins",
-      "2" => "Zwei",
-      "3" => "Drei",
-      "4" => "Vier",
-      "5" => "Fünf",
-      "6" => "Sechs",
-      "7" => "Sieben",
-      "8" => "Acht",
-      "9" => "Neun",
-    );
-
     public function initializeArguments()
     {
         $this->registerArgument('field', Field::class, 'powermail field', true);
@@ -37,15 +26,39 @@ class GuglerCaptchaViewHelper extends AbstractTagBasedViewHelper {
 
     public function render()
     {
-        $field = $this->arguments['field'];
+        $field = $this->arguments["field"];
 
-        $operator = array_keys($this->operators)[mt_rand(0, count($this->operators) - 1)];
-        $number1 = mt_rand(1, count($this->numbers));
-        $number2 = mt_rand(1, count($this->numbers));
-        $result = MathematicUtility::mathematicOperation($number1, $number2, $operator);
-        SessionUtility::setCaptchaSession((string)$result, $field->getUid());
+        $operator = array_keys($this->operators)[
+            mt_rand(0, count($this->operators) - 1)
+        ];
+        $number1 = mt_rand(1, 9);
+        $number2 = mt_rand(1, 9);
+        $result = MathematicUtility::mathematicOperation(
+            $number1,
+            $number2,
+            $operator
+        );
+        SessionUtility::setCaptchaSession((string) $result, $field->getUid());
 
-        return $this->numbers[$number1] . " " . $this->operators[$operator] . " " . $this->numbers[$number2] . " = ";
+        $number1Label = LocalizationUtility::translate(
+            "captcha.number." . $number1,
+            "gugler_powermail"
+        );
+        $number2Label = LocalizationUtility::translate(
+            "captcha.number." . $number2,
+            "gugler_powermail"
+        );
+        $operatorLabel = LocalizationUtility::translate(
+            "captcha.operator." . $this->operators[$operator],
+            "gugler_powermail"
+        );
+
+        return $number1Label .
+            " " .
+            $operatorLabel .
+            " " .
+            $number2Label .
+            " = ";
     }
 
 }
